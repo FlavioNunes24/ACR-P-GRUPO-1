3123b8873970998fcdcb6536a5f5432cc77fdc0c
Rúben Tiago Valente França <ruben_tiagof@hotmail.com>
2016-12-28 16:30:10 +0000

    Top músicas a funcionar

--- app/Http/Controllers/TopController.php
+++ app/Http/Controllers/TopController.php
@@ -3,11 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Musica;
+use App\Compra;
 
 class TopController extends Controller
 {
     public function index()
 	{
-		return view('top');
+		$musica = Musica::all();
+		$user=\Auth::user();
+		
+		if($user)
+		$compras = $user->compra()->get();
+		/*foreach($musica as $musicas)
+		{
+			$aux = $musicas->compra()->get();
+			$conta =count($aux);
+			echo $conta;
+		}*/
+		//$teste2= Musica::all();
+		/*$teste = Musica::with('compra')->get()->sortBy(function($teste2)
+													   {
+			dd($abc = $teste2->compra->count());
+		}); */
+		
+		($abc = Musica::withCount('compra')->orderBy('compra_count', 'desc')->paginate(10));
+		return view('top', compact('musica','abc','compras'));
 	}
 }
